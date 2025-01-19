a49aaaca53c5a69fa87f33ee42063145831af481
VolZa <vvolza@gmail.com>
2025-01-19 10:30:53 +0200

    startServer

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-import express from 'express';
+import { startServer } from './server.js';
 
-const PORT = 3000;
+// Запуск сервера
+startServer();
 
-const app = express();
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World! 1',
-  });
-
-});
-
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-  
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
