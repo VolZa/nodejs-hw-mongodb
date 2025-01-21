76990e0aacb225bd3859a7b7929e408a0add13a0
VolZa <vvolza@gmail.com>
2025-01-21 14:59:24 +0200

    refactoring contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,1 +1,10 @@
 import {ContactsCollection} from '../db/models/Contact.js';
+
+export const getAllContacts = () => ContactsCollection.find();    
+
+// export const getContactById = async (contactId) => {
+//     const contact = await ContactsCollection.findById(contactId);
+//     return contact;
+// };
+
+export const getContactById = (contactId) => ContactsCollection.findById(contactId);
