d3e7d75f5c260e7c9e7563739ba706b4217c7e14
VolZa <vvolza@gmail.com>
2025-02-13 20:06:30 +0200

    update package.json add eslint and prettier

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ export const startServer = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
-  app.use(cookieParser);
+  app.use(cookieParser());
 
   app.use(
     pino({
